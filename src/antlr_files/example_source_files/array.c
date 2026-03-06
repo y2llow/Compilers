47acc97032c86ba5e039c167df256d6bcbaf4726
Eray Ayyildiz <eray.aartselaar@gmail.com>
2026-03-06 15:05:38 +0100

    Updated the parser to include multi line comments (added hidden part for future LLVM comments) & added basic array parsing in the parser.g4 file

---
+++ src/antlr_files/example_source_files/array.c
@@ -0,0 +1,10 @@
+int main() {
+    int arr[5];
+    int values[3] = {10, 20, 30};
+    int matrix[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    int x = arr[0];
+    int y = matrix[1][2];
+    arr[2] = 42;
+    values[0] = 100;
+    return 0;
+}
